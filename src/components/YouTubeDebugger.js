df38ec73119a889d55c11c97a23410d86f1b19ed
Ngoc Dong Pham <cpud1258@gmail.com>
2018-10-01 11:37:06 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,20 @@
-// Code YouTubeDebugger Component Here
+// Code DigitalClicker Component Here
+import React from 'react';
+export default class  YouTubeDebugger extends React.Component {
+
+  constructor(props) {
+super(props);
+  this.state={ errors: [], user: null, settings: { bitrate: 8, video: { resolution: '1080p' } } };
+  
+  }
+  render(){
+    return(
+      <div>
+      <button className="bitrate" onClick={()=>{this.setState({settings.bitrate:12})}}>{this.state.timesClicked}</button>
+</div>
+<button className="bitrate" onClick={()=>{this.setState({timesClicked:this.state.timesClicked+1})}}>{this.state.timesClicked}</button>
+</div>
+    )
+  }
+}
+  
