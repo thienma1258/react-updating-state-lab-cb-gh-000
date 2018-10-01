f1651e0ff9f921eea697c5ae41171c6823c7a01e
Ngoc Dong Pham <cpud1258@gmail.com>
2018-10-01 11:31:00 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -10,7 +10,7 @@ super(props);
   }
   render(){
     return(
-      <button onClick={()=>{setState(timesClicked:timesClicked+1)}}>{this.state.timesClicked}</button>
+      <button onClick={()=>{setState(timesClicked:this.state.timesClicked+1)}}>{this.state.timesClicked}</button>
     )
   }
 }
